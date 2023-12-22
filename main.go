8872633f30579dae628d435c5ad0955f65a2bab9
masterpin <16904082+MasterPin@users.noreply.github.com>
2023-12-22 21:04:44 +0100

    added some todos for later

--- main.go
+++ main.go
@@ -7,6 +7,8 @@ import (
 
 func main() {
 
+	//todo: extract the tmpl rendering to its own file to be called from the main func later
+	//todo: implement a simple todo app that takes an input of items from the commandline and creates html or pdf from it.
 	t, err := template.New("hello_world").Parse("tmpls/hello_world.tmpl")
 	if err != nil {
 		panic(err)
