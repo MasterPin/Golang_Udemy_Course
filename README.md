f4f95ddb367fb6545e327c57b89ddcdc50e67048
masterpin <16904082+MasterPin@users.noreply.github.com>
2023-12-22 21:21:09 +0100

    Added mail.go. Read the Readme for further info

---
+++ README.md
@@ -0,0 +1,7 @@
+### Project Idea:
+## Todo-App with a Mail-Service
+
+I basically want to implement a basic todo app that sends you the daily todos as a email. <br>
+I also want to create pdf and html templating for the mails to make everything look nice <br>
+
+Maybe I would make the html for the mail itself and the pdf to have a downloadable pdf for printing out or something... lets see
--- main.go
+++ main.go
@@ -1,20 +1,16 @@
 package main
 
-import (
-	"html/template"
-	"os"
-)
-
 func main() {
 
 	//todo: extract the tmpl rendering to its own file to be called from the main func later
 	//todo: implement a simple todo app that takes an input of items from the commandline and creates html or pdf from it.
-	t, err := template.New("hello_world").Parse("tmpls/hello_world.tmpl")
-	if err != nil {
-		panic(err)
-	}
-	err = t.Execute(os.Stdout, t)
-	if err != nil {
-		panic(err)
-	}
+	//t, err := template.New("hello_world").Parse("tmpls/hello_world.tmpl")
+	//if err != nil {
+	//	panic(err)
+	//}
+	//err = t.Execute(os.Stdout, t)
+	//if err != nil {
+	//	panic(err)
+	//}
+
 }
