6f19938a73cea0a3b19f4af765e08b82baf8dd5f
masterpin <16904082+MasterPin@users.noreply.github.com>
2024-07-26 12:38:38 +0200

    enabled html template parsing through the application via template.ParseFiles

--- main.go
+++ main.go
@@ -1,14 +1,26 @@
 package main
 
 import (
-	"fmt"
+	"html/template"
+	"log"
 	"net/http"
 )
 
 type Strawberry int
 
-func (s Strawberry) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "Hello World!")
+var tpl *template.Template
+
+func (s Strawberry) ServeHTTP(w http.ResponseWriter, request *http.Request) {
+	err := request.ParseForm()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	tpl.ExecuteTemplate(w, "index.gohtml", request.Form)
+}
+
+func init() {
+	tpl = template.Must(template.ParseFiles("tmpls/index.gohtml"))
 }
 
 func main() {
